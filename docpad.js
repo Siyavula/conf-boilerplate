e4c35f4142320791c97283f44f22889bc9c1ace3
Mark Horner <mark@siyavula.com>
2016-05-17 05:34:19 +0200

    Updates

--- docpad.js
+++ docpad.js
@@ -157,14 +157,14 @@ module.exports = {
       {
         name: "Kosma von Maltitz",
         photo: "themes/yellow-swan/img/Kosma_von_Maltitz.jpg",
-        bio: "TBC",
+        bio: "Kosma has a MSc in Astrophysics and Space Science. At Siyavula she coordinates the EdTech Production Team and creates Physics content. Kosma also presents shows at the Iziko Planetarium and does Astronomy outreach whenever she gets a chance.",
         company: "Siyavula Education",
         link: {
-          href: "http://twitter.com/rjallain",
-          text: "@rjallain"
+          href: "http://twitter.com/kosmavm",
+          text: "@kosmavm"
         },
         presentation: {
-          title: "TBC",
+          title: "Exo-planets",
           description: "In the spirit of - exo-planets, recent discoveries",
           time: "17h30"
         }
@@ -197,7 +197,7 @@ module.exports = {
           text: "@carolune"
         },
         presentation: {
-          title: "TBC",
+          title: "Once a physicist, always a physicist and the opportunities outside of academia",
           description: "In the spirit of - once a physicist, always a physicist and the opportunities outside of academia",
           time: "9h30"
         }
@@ -212,8 +212,8 @@ module.exports = {
           text: "@asdf"
         },
         presentation: {
-          title: "TBC",
-          description: "",
+          title: "The importance of computational physics, it's key role in research and what this means for teaching physics",
+          description: "In the spirit of - the importance of computational physics, it's key role in research and what this means for teaching physics",
           time: "10h30"
         }
       },
@@ -231,8 +231,8 @@ module.exports = {
           text: "@billy95"
         },
         presentation: {
-          title: "An update on PER",
-          description: "dfdfdafasf",
+          title: "Physics Education Research (PER)",
+          description: "In the spirit of - what research is happening, what should teachers be aware of and where can they contribute",
           time: "12h00"
         }
       },
@@ -243,7 +243,7 @@ module.exports = {
       {
         name: "Megan Beckett",
         photo: "themes/yellow-swan/img/Megan_Beckett.jpg",
-        bio: "Coming soon.",
+        bio: "With a MSc in Molecular Biology, Megan followed her goal to make STEM education engaging, relevant and accessible. She leads the research, analytics and pedagogical design for Siyavula's current and future e-learning experiences. She works to advance open education.",
         company: "Siyavula Education",
         link: {
           href: "http://twitter.com/meganbeckett2",
@@ -252,13 +252,13 @@ module.exports = {
         presentation: {
           title: "Completing the Circuit 1 - Connecting components in the electric circuits teaching-learning cycle",
           description: "This workshop aims to look at some specific learning strategies that can be applied to the learning (and teaching) of electric circuits in Grades 10 to 12 Physical Sciences. Some specific concepts in electric circuits theory (e.g. potential difference; emf and internal resistance; resistance) will be included as discussion points. The purpose of including specific topics is not to teach any content, but rather to discuss how we teach the content. What analogies do we use? How do we sequence the concepts? What are the potential advantages and disadvantages that result from any particular decision we make? This workshop will be very much hands-on and interactive.",
-          time: "15h00"
+          time: "14h00"
         }
       },
       {
         name: "Adam Reynolds",
         photo: "themes/yellow-swan/img/Adam_Reynolds.jpg",
-        bio: "Coming soon.",
+        bio: "Adam has a BSc (majoring in Physics, Astrophysics, Mathematics & Applied Mathematics) and a PGCE from UCT. In addition to being an Educational Technologist he still teaches Physical Sciences part-time and is also a matric marker for Physics.",
         company: "Siyavula Education",
         link: {
           href: "http://github.com/educationmonger",
@@ -277,7 +277,7 @@ module.exports = {
       {
         name: "Mark Horner",
         photo: "themes/yellow-swan/img/Mark_Horner.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        bio: "Mark has a Ph.D. in Nuclear Physics and is a former Shuttleworth Fellow. He initiated the Free High School Science Texts project way back in 2002. This formed the foundation upon which Siyavula is built today.",
         company: "Facebook",
         link: {
           href: "http://twitter.com/marknewlyn",
